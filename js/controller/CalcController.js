6315c94396396ab7bb1d1feab6c7247cd6bae3cb
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-11-04 00:00:37 -0300

    Criando um método para adicionar multiplos eventos nos btns

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -3,6 +3,45 @@ class CalcController {
     constructor(){
 
         this._displayCalc = '0' ;
+        this.inicialize();
+        this.initEventButtosn();
+    }
+
+
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach( event => {
+
+            element.addEventListener(event, fn, false);
+        }
+
+        )
+    }
+
+    initEventButtosn(){
+
+        let buttons = document.querySelectorAll(".btn");
+        
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, 'click drag', e => {
+                
+                console.log(btn.className.replace("btn-",""));
+            });
+
+
+            this.addEventListenerAll(btn, 'mouseup mouseover mousedown', e => {
+
+                btn.style.cursor = 'pointer';
+            });
+        });
+    }
+
+
+    inicialize(){
+        let displayCalcEl = document.querySelector("#display");
+
+        displayCalcEl.innerHTML = "123456";
     }
 
 
@@ -15,6 +54,6 @@ class CalcController {
     set displayCalc(value){
          
         this._displayCalc = value;
-        
+
     }
 }
